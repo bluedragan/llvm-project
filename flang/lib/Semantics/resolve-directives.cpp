64092a7d1f45eeae65f733bf017b0b70b42ce264
agozillon <Andrew.Gozillon@amd.com>
2025-11-12 17:41:51 -0600

    [Flang] Fix defaultmap(none) being overly aggressive with symbol checks
    Currently we're picking up and complaining about builtin symbols like
    Null() when defaultmap(none) is set, so I've relaxed the restriction a
    bit to allow for procedures and named constants to bypass the
    restriction. It might be the case that we want to tighten it up again
    in certain aspects in the future.

--- flang/lib/Semantics/resolve-directives.cpp
+++ flang/lib/Semantics/resolve-directives.cpp
@@ -3072,7 +3072,8 @@ void OmpAttributeVisitor::Post(const parser::Name &name) {
       if (Symbol * found{currScope().FindSymbol(name.source)}) {
         // If the variable has declare target applied to it (enter or link) it
         // is exempt from defaultmap(none) restrictions
-        if (!symbol->GetUltimate().test(Symbol::Flag::OmpDeclareTarget)) {
+        if (!symbol->GetUltimate().test(Symbol::Flag::OmpDeclareTarget) &&
+            !IsProcedure(*symbol) && !IsNamedConstant(*symbol)) {
           auto &dMap = GetContext().defaultMap;
           for (auto defaults : dMap) {
             if (defaults.second ==
